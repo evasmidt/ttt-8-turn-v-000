4264bea21e4c13302d08bb77997d0e9262957b65
Eva Smidt <shevangeline@gmail.com>
2018-01-29 00:21:28 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,6 +31,7 @@ def turn(board)
   #valid_move?(board, index) == false
     puts "Please enter 1-9:"
     input = gets.strip
+      index = input_to_index(input)
    display_board(board)
   #input_to_index
   #between?(0,8)
