db2b5901cdb6faf22bc7c561d283a5947665e8c0
Eva Smidt <shevangeline@gmail.com>
2018-01-29 00:46:58 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,7 +41,7 @@ def turn(board)
 
 
   valid_move?(board, index) == false
-  until valid_move?(board, index) < 9 && => 0
+  until input >= 0 && input <= 9
     puts "Invalid. Please enter 1-9:"
     valid_move?(board, index) += 1
   end
